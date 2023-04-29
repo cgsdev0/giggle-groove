75bfc9219fbef7f44585997186cd7cf0c3089a3e
Sarah Schulte <git@sarah.engineer>
2023-04-29 11:30:45 -0700

    is this gameplay?

--- not_joke.gd
+++ not_joke.gd
@@ -11,26 +11,40 @@ func start_joke():
 		$Words.remove_child(child)
 		child.queue_free()
 		
+	var shift = 0
+	var last_anchor_right = 0.0
 	for label in joke.labels:
 		var p = Panel.new()
 		p.set_script(JokeWord)
 		p.controller = self
+		p.label = label
 		p.anchor_left = label.start / joke.audio.get_length()
-		p.anchor_right = 1 # label.end / joke.audio.get_length()
+		p.anchor_right = label.end / joke.audio.get_length()
+		if (p.anchor_left < last_anchor_right || randf_range(1.0, 100.0) < 50.0):
+			shift = (shift + 1) % 2
+		p.row = shift
+		p.anchor_top = 0.1 + shift * 0.3
+		p.anchor_bottom = 0.4 + shift * 0.3
 		var l = RichTextLabel.new()
+		l.autowrap_mode = TextServer.AUTOWRAP_OFF
 		l.fit_content = true
 		l.scroll_active = false
 		l.layout_mode = 1
 		l.anchors_preset = Control.LayoutPreset.PRESET_FULL_RECT
 		l.bbcode_enabled = true
-		var text = "[color=red]" + label.label[0] + "[/color]" + label.label.substr(1)
-		l.text = text
 		p.add_child(l)
 		$Words.add_child(p)
+		last_anchor_right = p.anchor_right
 	$AudioStreamPlayer.stream = joke.audio
+	await get_tree().create_timer(1.0).timeout
 	$AudioStreamPlayer.play()
 
-
+func _unhandled_key_input(event):
+	if event.is_action_pressed("row0"):
+		print("MISCLICK")
+	if event.is_action_pressed("row1"):
+		print("MISCLICK")
+		
 func get_progress():
 	return $AudioStreamPlayer.get_playback_position() / joke.audio.get_length()
 	
--- scripts/joke_word.gd
+++ scripts/joke_word.gd
@@ -2,12 +2,62 @@ class_name JokeWord extends Control
 
 
 var controller: JokeController
+var label: AudioLabel
+
+var highlighted = false
+var row = 0
+var string_size
+var hit = false
+var missed = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var l = get_child(0)
+	string_size = l.get_theme_font("font").get_string_size(label.label, HORIZONTAL_ALIGNMENT_LEFT, -1, l.get_theme_font_size("font_size"))
+	var initial_size = anchor_right - anchor_left
+	var min_size = (string_size.x + 16) / controller.get_rect().size.x
+	if min_size > initial_size:
+		anchor_right = anchor_left + min_size
+		
+	default_text()
+	
+func default_text():
+	var text = label.label # "[color=red]" + label.label[0] + "[/color]" + label.label.substr(1)
+	get_child(0).text = text
+	
+func highlight_text():
+	var text = "[color=yellow]" + label.label + "[/color]"
+	get_child(0).text = text
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+const SLOP = 0.01
+func _input(event):
+	if get_viewport().is_input_handled():
+		return
+	# input handler for hitting the right key
+	if controller.get_progress() > anchor_left - SLOP && controller.get_progress() < anchor_right + SLOP:
+		if event.is_action_pressed("row" + str(row)) && !hit && !missed:
+			get_viewport().set_input_as_handled()
+			hit = true
+			theme_type_variation = "HitJoke"
+			return
+	
+	if controller.get_progress() > anchor_left + SLOP && controller.get_progress() < anchor_right - SLOP:
+		# Check if the wrong key was hit; this overrides a success
+		if event.is_action_pressed("row" + str((row + 1) % 2)) || (event.is_action_pressed("row" + str(row)) && hit):
+			hit = false
+			missed = true
+			theme_type_variation = "MissedJoke"
+			
 func _process(delta):
-	if controller.get_progress() > anchor_left:
-		get_child(0).text = ""
+	
+	if controller.get_progress() > anchor_left && controller.get_progress() < anchor_right:
+		# Highlight when cursor over
+		if !highlighted && !hit:
+			highlighted = true
+			highlight_text()
+	
+	# unhiglight after cursor leaves
+	if controller.get_progress() > anchor_right && highlighted:
+		if !hit:
+			theme_type_variation = "MissedJoke"
+		highlighted = false
+		default_text() 
